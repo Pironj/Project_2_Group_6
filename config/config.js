a7a809ad1bd09cfbb5268baf6c64287f506439a0
Julian Piron <pironj27@gmail.com>
2019-06-21 07:41:30 -0700

    deploy to heroku

--- config/config.js
+++ config/config.js
@@ -1,28 +1,24 @@
 require("dotenv").config();
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  module.exports = {
-    development: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      dialect: "mysql",
-      port: process.env.DB_PORT
-    },
-    test: {
-      username: "root",
-      password: null,
-      database: "testdb",
-      host: "localhost",
-      dialect: "mysql",
-      logging: false
-    },
-    production: {
-      use_env_variable: "JAWSDB_URL",
-      dialect: "mysql"
-    }
-  };
-}
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
